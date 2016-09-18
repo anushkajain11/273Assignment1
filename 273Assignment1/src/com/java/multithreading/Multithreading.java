c0097a81f38128d1e8728176766178c4911fa374
anushkajain11 <anushka.jain1499@gmail.com>
2016-09-18 16:59:36 -0700

    Java-Multithreading

--- 273Assignment1/src/com/java/multithreading/Multithreading.java
+++ 273Assignment1/src/com/java/multithreading/Multithreading.java
@@ -12,15 +12,12 @@ public class Multithreading implements Runnable{
 		
 		public synchronized void run() {
 			
-			//System.out.println("Running :" + this.Name);
-					
 			for (int i=0; i<10;i++){
 				
 				System.out.println("Running:" + this.Name + "  Value : " + c.setValue(i));
 				try {
 					Thread.sleep(10);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
